e8dccf84808eb1ed9009afe2cbc62072e3010ba1
Ahmad Rifa'i <personal.rifai@gmail.com>
2023-10-27 09:40:43 +0700

    feat: create constants

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+const String people = 'assets/lottiefiles/people.zip';
+const String gitHub = 'assets/svgs/github.svg';
+const String linkedIn = 'assets/svgs/linkedin.svg';
+const String gitHubLink = 'https://github.com/arifai';
+const String linkedInLink = 'https://www.linkedin.com/in/ahmad-rifai/';
